dc0c5ff28af1674666ecfcce6c2afb21e34abdca
Martin Dekov <mvdekov@gmail.com>
2020-05-13 01:51:47 +0300

    Move to using GitHub SDK
    Moving the code to use the Python SDK for GitHub
    instead of directly calling the API
    
    Signed-off-by: Martin Dekov <mvdekov@gmail.com>

--- lockbot/handler.py
+++ lockbot/handler.py
@@ -1,51 +1,42 @@
 import os
-import requests
 from datetime import datetime
+from github import Github
 
 
-def get_difference_in_days(string_date):
-    # GitHub date format: 2019-01-13T21:17:09Z
-    created_date = string_date.split('T')[0].split('-')
-    for i in range(0, len(created_date)):
-        created_date[i] = int(created_date[i])
-    created_date = datetime(created_date[0], created_date[1], created_date[2])
-    return datetime.now() - created_date
-
-
-def lock_issue(issue_number, auth_header, username, repo):
-    r = requests.put(
-        url=f"https://api.github.com/repos/{username}/{repo}/issues/{issue_number}/lock", headers=auth_header)
-    if r.status_code == 204:
-        return f"Issue: {issue_number} locked"
-
-
-def get_authorization_header():
+def get_issues():
+    desired_repo = os.getenv("github_repository")
+    desired_days = int(os.getenv("inactive_days"))
+    auth = None
     with open("/var/openfaas/secrets/auth-token") as file:
-        return {'Authorization': f"token {file.read().strip()}"}
+        auth = Github(file.read().strip())
+
+    issues_for_lock = []
+    for repo in auth.get_user().get_repos():
+        if repo.name == desired_repo:
+            for issue in repo.get_issues():
+                if not issue.pull_request and not issue.locked:
+                    last_comment = issue.get_comments()[
+                        issue.comments-1].updated_at
+                    difference = datetime.now() - datetime(last_comment.year,
+                                                           last_comment.month,
+                                                           last_comment.day)
+                    if difference.days > desired_days:
+                        issues_for_lock.append(issue)
+
+    return issues_for_lock
+
+
+def lock(issues):
+    response = "no unlocked inactive issues"
+    if len(issues) > 0:
+        response = "issues locked:"
+        for issue in issues:
+            issue.lock("off-topic")
+            response = response + f"\n{issue.title}"
+    return response
 
 
 def handle(req):
-    username = os.getenv("github_username")
-    repo = os.getenv("github_repository")
-    days_before_outdated = int(os.getenv("days_before_outdated"))
-    r = requests.get(f"https://api.github.com/repos/{username}/{repo}/issues")
-    issues = r.json()
-
-    issue_numbers = []
-    for issue in issues:
-        if not issue.get('pull_request'):
-            diff = get_difference_in_days(issue.get('created_at'))
-            if diff.days > days_before_outdated and not issue.get("locked"):
-                issue_numbers.append(int(issue.get('number')))
-
-    locked_issues = ""
-    if len(issue_numbers) > 0:
-        auth_header = get_authorization_header()
-        for issue_number in issue_numbers:
-            locked_issues = locked_issues + \
-                f"{lock_issue(issue_number, auth_header, username, repo)}\n"
-
-    if locked_issues != "":
-        return locked_issues
-
-    return "No issues locked"
+    issues = get_issues()
+    response = lock(issues)
+    return response
